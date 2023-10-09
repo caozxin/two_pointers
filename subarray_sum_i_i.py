f69cfcaea23d4492476e699bb91dd80f52479147
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2023-10-09 15:20:40 -0400

    need to revisit

--- subarray_sum_i_i.py
+++ subarray_sum_i_i.py
@@ -25,39 +25,30 @@ class Solution:
         # use list to track all sums from those subarrays
         # find and count if each_sum from the sum_dict() is in given sum array [start, end]
         
-        # find all subarray of a list of integers:
+        # please note this question needs to be in-place
         n = len(a)
         if n <=0:
             return 0
-        sum_list = []
+        # sum_list = []
 
         #check if each_sum in sum_list is in the given_sum_array
-        given_sum_array = range(start, end + 1)
-        print("given_sum_array", given_sum_array)
-        sum_dict = dict()
-        for each_sum in given_sum_array:
-            sum_dict[each_sum] = 0
+        # given_sum_array = range(start, end + 1)
+        # print("given_sum_array", given_sum_array)
 
         counter = 0
 
         for i in range(n):
-            # i in the length of the subarray
-            # new_sub_arr = []
+
             left, right = 0, 0 + i
             while right < n:
                 
                 new_sub_arr = a[left:right+1]
-                sum_list = sum(new_sub_arr)
-                if sum_list in given_sum_array:
+                sum_list = sum(new_sub_arr) # this is costly
+                if start <= sum_list <= end: 
                     counter += 1
                 left += 1
                 right = left + i
-                print("left, right, new_sub_arr")
-                print(left, right, new_sub_arr) 
-
-        # print("sum_list", sum_list, len(sum_list))
-
-        
+   
         
         return counter
 
@@ -102,11 +93,128 @@ class Solution:
                     count += 1
 
         return count
+    
+    def subarray_sum_i_i_hasmap(self, a: List[int], start: int, end: int) -> int:
+        # write your code here
+        # find all subarray of a list of integers
+        # use list to track all sums from those subarrays
+        # find and count if each_sum from the sum_dict() is in given sum array [start, end]
+        
+        # find all subarray of a list of integers:
+        n = len(a)
+        if n <=0:
+            return 0
+        sum_list = []
+
+        #check if each_sum in sum_list is in the given_sum_array
+        given_sum_array = range(start, end + 1)
+        # print("given_sum_array", given_sum_array)
+        sum_dict = dict()
+        for each_sum in given_sum_array:
+            if each_sum in given_sum_array:
+                sum_dict[each_sum] = 0
+        # print("sum_dict", sum_dict)
+        # exit()
+        counter = 0
+
+        for i in range(n):
+            # i in the length of the subarray
+            # new_sub_arr = []
+            left, right = 0, 0 + i
+            while right < n:
+                
+                new_sub_arr = a[left:right+1]
+                sum_list = sum(new_sub_arr)
+                # print("sum_list", sum_list)
+                if sum_dict.get(sum_list) == 0:
+                    # print("in")
+                    counter += 1
+                left += 1
+                right = left + i
+                # print("left, right, new_sub_arr")
+                # print(left, right, new_sub_arr) 
+        return counter
+        # print("sum_list", sum_list, len(sum_list))
+
+    def subarray_sum_i_i_without_sum(self, a: List[int], start: int, end: int) -> int:
+        # write your code here
+        # find all subarray of a list of integers
+        # use list to track all sums from those subarrays
+        # find and count if each_sum from the sum_dict() is in given sum array [start, end]
+        
+        # please note this question needs to be in-place
+        n = len(a)
+        if n <=0:
+            return 0
+
+        counter = 0
+
+        for i in range(n):
+
+            left, right = i, i
+            curr_sum = 0 
+            while right < n:
+                curr_sum += a[right]
+                # new_sub_arr = a[left:right+1]
+                # sum_list = sum(new_sub_arr) # this is costly
+                # print("new_sub_arr", new_sub_arr)
+                print("curr_sum", curr_sum)
+                if start <= curr_sum <= end: # this is costly
+                    print("in")
+                    counter += 1
+                if curr_sum > end:
+                    break
+                right +=1 
+   
+        
+        return counter
+    def subarray_sum_i_i_amazing(self, a: List[int], start: int, end: int) -> int: #上下界问题转化为只有上界的问题
+        def check(x):
+            i, s, ans = 0, 0, 0
+            for k in range(len(a)):
+                s += a[k]
+                while(i<=k and s>x):
+                    s -= a[i]
+                    i += 1
+                ans += k-i+1
+            return ans
+        return check(end) - check(start-1)
+    def subarray_sum_i_i05(self, a: List[int], start: int, end: int) -> int:
+
+        n = len(a)
+        if n <=0:
+            return 0
+
+        counter = 0
+
+        for i in range(n):
+
+            left, right = 0, 0 + i
+            while right < n:
+                
+                new_sub_arr = a[left:right+1]
+                # new_sum = sum(a[:(right +1)]) - sum(a[:left])
+                # print(new_sub_arr, a[:(right +1)], a[:left])
+                # if a[left] + a[right] < end:
+                #     break
+                if len(new_sub_arr) >= 2 and a[right] >= end:
+                    break
+                if len(new_sub_arr) == 1 and a[left] > end:
+                    break
+                print(new_sub_arr)
+                sum_list = sum(new_sub_arr) # this is costly
+                if start <= sum_list <= end: 
+                    counter += 1
+                left += 1
+                right = left + i
+   
+        
+        return counter
 
 new_solution = Solution()
 A = [1, 2, 3, 4, 5]
-start = 3
-end = 7
+start = 1
+end = 3
 result = new_solution.subarray_sum_i_i03(A, start, end)
 
 print("result", result)
