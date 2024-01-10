99cc53f675c4079c4e054eb271c1017d1085faa1
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-01-09 22:39:58 -0500

    Create maximum_subarray_sum_with_fixed_size.py

---
+++ maximum_subarray_sum_with_fixed_size.py
@@ -0,0 +1,3 @@
+#Given an array (list) nums consisted of only non-negative integers, find the largest sum among all subarrays of length k in nums.
+
+#For example, if the input is nums = [1, 2, 3, 7, 4, 1], k = 3, then the output would be 14 as the largest length 3 subarray sum is given by [3, 7, 4] which sums to 14.
