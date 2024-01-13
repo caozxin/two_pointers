885f348064e485c40bd08712dd4ea1d99677201c
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-01-13 07:12:35 -0500

    Update subarray_sum_longest.py

--- subarray_sum_longest.py
+++ subarray_sum_longest.py
@@ -4,31 +4,33 @@ from typing import List
 
 def subarray_sum_longest(nums: List[int], target: int) -> int:
     # WRITE YOUR BRILLIANT CODE HERE
-    print("input", nums, target)
+#     print("input", nums, target)
     if not nums: 
         return 0
     n = len(nums)
-    k = 1 # k = length of the substring
-    max_length = 0 
-    window = nums[:k]
-    curr_sum = sum(window)
     left, right = 0, 0
-    while right < n and left <= right: 
-        
-        window = nums[left:right+1]
-        print(left, right, window)
-        
-        #             curr_sum = curr_sum - window[0] + window[-1] # curr_sum did not get update properly
-        curr_sum -= nums[left]
+    curr_sum = 0
+    max_len = 0 
+    
+    for right in range(n):
         curr_sum += nums[right]
-        print("    curr_sum", curr_sum)
-        print("    window_sum", sum(window))
+        window = nums[left: right+1]
+#         print("*curr_sum", curr_sum)
+        curr_len = right - left + 1
+#         print("*window", window)
+#         print("*curr_len", curr_len)
         
-        if curr_sum <= target:
-            right += 1 # left and right are the same direction, you update right when curr_sum < target; otherwise update left.
-            
+        if curr_sum > target:
+            left += 1
+            curr_sum -= nums[left]
         else:
-            exit()
-            left += 1 
-        max_length = max(max_length, len(window))
-        print("max_length", max_length)
+            max_len = max(max_len, curr_len)
+            right += 1
+#         print("window", window)
+#         window = nums[left: right+1]
+        
+#         print('curr_len', left, right, curr_len)
+#         print("    window", window)
+        
+#         print('    max_len', max_len)
+    return max_len
