d0228f02a4633ebe493d44ab6d0ce24dc5f630d4
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-01-15 02:33:13 -0500

    Update length_of_longest_substring.py

--- length_of_longest_substring.py
+++ length_of_longest_substring.py
@@ -7,23 +7,60 @@ class Solution:
     example = abcabcbb -->3,  The longest substring is "abc".
     example = bbbbb -->1,  The longest substring is "b".
     """
-    def length_of_longest_substring(self, s: str) -> int:
+    def length_of_longest_substring00(self, s: str) -> int:
         # write your code here
         if not s:
             return 0
 
-        left, right = 0, 0
-        n = len(s)
+        left, right = 0, 1
+        input_string = s
+
+        n = len(input_string)
         longest_len = 0
 
-        for right in range(n):
+        for right in range(1, n):
             
-            right += 1
-
-            while s[left] == s[right] and left < n:
-                left += 1
+            # the window here is substring with non-repeating characters!
+            window = input_string[left:right]
             print("left, right", left, right)
+            # print("window", window)
+            #1st check on outside dups:
+            while input_string[left] == input_string[right] and left < n:
+                print(" *outside has dups")
+                left += 1
+                # print(left, right, window)
+            
+            #2) check on inside dups:
+            while input_string[right] in input_string[left:right] and input_string[left] != input_string[right] and left < n:
+                print(" *inside has dups")
+                left += 1
+
+            # print("left, right", left, right)
             curr_len = right - left + 1
             print("curr_len", curr_len)
             longest_len = max(longest_len, curr_len)
             print("longest_len", longest_len)
+        # exit()
+        return longest_len
+
+
+    def length_of_longest_substring(self, s: str) -> int:
+        if not s:
+            return 0
+
+        left, right = 0, 0  # Initialize both pointers at the beginning of the string
+        n = len(s)
+        longest_len = 0
+        unqiue_char_set = set()  # Use a set to keep track of unique characters in the current window
+
+        while right < n:
+            if s[right] not in unqiue_char_set:
+                unqiue_char_set.add(s[right])
+                right += 1
+                longest_len = max(longest_len, right - left)
+            else:
+                unqiue_char_set.remove(s[left])
+                left += 1
+
+        return longest_len
+
