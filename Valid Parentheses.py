1a609343dee1dfa13736d69c3f60548531e476c8
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-13 03:01:30 -0400

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,3 +1,34 @@
-#Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+class Solution:
+    """
+    @param s: A string
+    @return: whether the string is a valid parentheses
+    """
+    def is_valid_parentheses(self, s: str) -> bool:
+        # write your code here
+        if not s:
+            return
 
-#The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+        matching_dict = {"(":")", "[":"]", "{":"}"}
+
+        fast, slow = 0, 0 
+
+        n = len(s)
+
+        if n == 1:
+            return False
+
+        while fast < len(s):
+            print("b4", slow, fast)
+            if slow == fast:
+                print("1", slow, fast)
+                pass
+            else:
+                if matching_dict.get(s[slow]) == s[fast]:
+                    print("2", slow, fast)
+                    pass 
+                else:
+                    print("3", slow, fast)
+                    return False
+            fast +=1
+        
+        return True
