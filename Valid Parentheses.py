1ae1bf436f72b70a66c046c7a428578fe1fc1f6f
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-16 22:11:44 -0400

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -8,29 +8,29 @@ class Solution:
         if not s:
             return
 
-        matching_dict = {"(":")", "[":"]", "{":"}"}
-
-        fast, slow = 0, 0 
-
         n = len(s)
 
         if n % 2 == 1: # it means odd number of items
             return False
 
-        for slow in range (len(s)):
+        matching_dict = {"(":")", "[":"]", "{":"}"}
+        stack = []
 
-            if matching_dict.get(s[slow]): # it finds the first half of any qualified parentheses
-                print("matching", s[slow])
-                fast = slow + 1
+        for pointer in range (len(s)):
 
-                while fast < len(s) and fast > slow:
+            if matching_dict.get(s[pointer]): # it finds the first half of any qualified parentheses
+                # print("matching", s[slow])
+                stack.append(s[pointer]) # only append to stack when it is first half
+            else:
+                if not stack:
+                    return False # when stack has no any first half
+                expected = stack.pop()
+                if s[pointer] != matching_dict[expected]: # if last first half in stack does not matching it
+                    return False
 
-                    if s[fast] !=  matching_dict.get(s[slow]):
-                        # print("find it", s[fast])
-                        print("not pairng")
-                        # return False
-                    fast += 1
-
-        # return True
+        if not stack:
+            return True
+        else:
+            return False
 
             
