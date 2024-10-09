ddb5c943ca0996bc907f085d6b5f63ae34d9177b
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-10-09 10:55:28 -0400

    Update Reverse Words in a String.py

--- Reverse Words in a String.py
+++ Reverse Words in a String.py
@@ -1,1 +1,18 @@
 #Reverse Words in a String
+#first version: using Python built-in functions
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        #first version:
+        res = s.split(' ')
+        # print(res)
+
+        post_res = []
+        for each in res:
+            if each:
+                post_res.append(each)
+        # print(post_res)
+        # print(reversed(res))
+        final_res = None
+        final_res = ' '.join(reversed(post_res))
+        # print(final_res)
+        return final_res
