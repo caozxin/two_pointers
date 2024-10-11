ddd385f6ad4acc53bbb9d8b268c7a26933d16ffb
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-10-11 05:40:30 -0400

    Update Valid Word Abbreviation.py

--- Valid Word Abbreviation.py
+++ Valid Word Abbreviation.py
@@ -3,6 +3,7 @@ class Solution:
         
         test_word = '55'
         # print("test", test_word.isdigit())
+        print("max_i, max_j", len(word), len(abbr))
 
 
         if len(word) < len(abbr):
@@ -10,22 +11,30 @@ class Solution:
 
         i, j = 0, 0
 
-        while j < len(abbr):
+        while j < len(abbr) and i < len(word):
             # print(abbr[j])
             step = 1
             if abbr[j].isdigit():
+                if abbr[j] == '0': # any leading zero we return False
+                    return False
 
-                while abbr[j:j + step].isdigit():
+                while abbr[j:j + step].isdigit() and step < len(abbr) : # here we could change it for abbr[j + step].isdigit()
                     step += 1
+                i_steps = int(abbr[j:j + step-1])
+                print("j+step", int(abbr[j:j + step-1]), i_steps)
+                i += i_steps 
+                print("i, j", i, j)
 
-                print("j+step", abbr[j:j + step-1])
-                j = j + step - 1
+                j = j + step - 1 # we park j for now
+                if j >= len(abbr) or i >= len(word):
+                    return False
+                print("i, j", i, j, word[i], abbr[j])
 
-            # exit()
-            if word[i] != word[j]:
+            print("i, j", i, j, word[i], abbr[j])
+            if word[i] != abbr[j]:
                 return False
-
+            # print(i, j)
             j += 1
             i += 1
-            
-            # return False
+
+        return True
