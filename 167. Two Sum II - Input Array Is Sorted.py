effcc7abbf47b982ed91759bb0a76a46ee7f9ce8
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-06-19 11:33:09 -0400

    Create 167. Two Sum II - Input Array Is Sorted.py

---
+++ 167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # none handling:
+        if not numbers:
+            return []
+
+        n = len(numbers)
+        left, right = 0, n-1 # final return should be [left+1, right+1]
+
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right+1]
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else: 
+                left += 1
+
+        return []
