334fbcd30290a912033f9ec5589d7ba65b48317c
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-06-19 10:27:54 -0400

    Create 876. Middle of the Linked List.py

---
+++ 876. Middle of the Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #none handeling:
+        if not head or not head.next:
+            return head
+
+        slow, fast = head, head
+        # while fast:
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
