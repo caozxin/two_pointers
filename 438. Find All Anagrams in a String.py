d5cac8c49cd6978d36b5226eaf48f9ad8f0f6fd6
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-06-27 06:20:15 -0400

    Update 438. Find All Anagrams in a String.py

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -27,3 +27,31 @@ class Solution:
         return result
 
 
+### improved version:
+from collections import Counter
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        #none handling:
+        if not s or not p:
+            return []
+        result = []
+        # the key is to find the matching substring that has the same Counter() per char with the target_p.
+        # for slicing window technqiue, we do not directly defin the window. 
+        window_count = Counter() # initiate the window_count
+        # print(s[:len(p)], window_count)
+        # exit()
+
+        for right in range(len(p), len(s)+1):
+            print(s[:right])
+            exit()
+            left = right - len(p)
+            # print(left, right, s[left:right])
+            window =  s[left:right] # this is the problem. For slicing window technique, we do not directly define the window. 
+            #  print(left, right, window)
+            # # print(check_anagram(window, p))#
+            if check_anagram(window, p):
+                result.append(left)
+        return result
+
+
