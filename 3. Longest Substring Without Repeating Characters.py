82399e161aa79aba12c34e4244496787af2e7c40
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-07-04 11:39:09 -0400

    Update 3. Longest Substring Without Repeating Characters.py

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import List
 
 class Solution:
@@ -8,21 +8,16 @@ class Solution:
 
         max_len = 0
         left = 0
-        window_count = Counter(s)
-        # print(window_count)
-        for each in window_count:
-            # print(each)
-            window_count[each] = 0
-        # print(window_count)
+        window_count = defaultdict(int) # we need to use this to avoid non-key error
+
         for right in range(len(s)):
             window_count[s[right]] += 1
-            # print(window_count)
+
             while window_count[s[right]] > 1:
                 window_count[s[left]] -= 1 
                 left += 1
             max_len = max(max_len, right + 1 - left)
-            # print(max_len, s[left:right+1])
-        # exit()
+
 
         return max_len
 
